a37e677d3b293137585df9c52f12e693528feb21
Sam <375162+samtarling@users.noreply.github.com>
2021-08-28 03:34:37 +0100

    +

--- index.php
+++ index.php
@@ -1,3 +1,56 @@
+<?php
+if (isset($_GET['susmuffin'])) {
+    ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+    <title>TheresNoTime for Susmuffin</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-205260692-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-205260692-1');
+    </script>
+  </head>
+  <body>
+    <div class='container text-center'>
+        <div class='row'>
+            <div class='col-sm pt-5'>
+                <h1 class='display-2'>You're a bit weird tho</h1>
+            </div>
+        </div>
+
+        <div class='row'>
+            <div class='col'></div>
+            <div class='col'>
+                <img src='img/pfp2.png' class='img-fluid rounded'>
+            </div>
+            <div class='col'></div>
+        </div>
+
+        <div class='row pt-3'>
+            <div class='col-sm-3'></div>
+            <div class='col-sm-6'>
+                <p class='lead'>
+                    If I cared more I'd think up a scathing remark
+                </p>
+            </div>
+            <div class='col-sm-3'></div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
+    <?php
+} else {
+    ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -59,3 +112,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
+    <?php
+}
+?>
