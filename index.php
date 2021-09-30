18a0c016b4dbc76c2f585a5d972395b1498e0318
Sam <375162+samtarling@users.noreply.github.com>
2021-09-30 16:29:05 +0100

    little updates

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-if (isset($_GET['susmuffin'])) {
-    ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -8,7 +5,7 @@ if (isset($_GET['susmuffin'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
-    <title>TheresNoTime for Susmuffin</title>
+    <title>TheresNoTime | 👋</title>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-205260692-1"></script>
     <script>
@@ -22,14 +19,14 @@ if (isset($_GET['susmuffin'])) {
   <body>
     <div class='container text-center'>
         <div class='row'>
-            <div class='col-sm pt-5'>
-                <h1 class='display-2'>You're a bit weird tho</h1>
+            <div class='col-sm pt-4'>
+                <h1 class='display-1 title-bit'>Hey, nice to meet you!</h1>
             </div>
         </div>
 
         <div class='row'>
             <div class='col'></div>
-            <div class='col'>
+            <div class='col-sm-5 col-md-4'>
                 <img src='img/pfp5.png' class='img-fluid rounded'>
             </div>
             <div class='col'></div>
@@ -39,60 +36,10 @@ if (isset($_GET['susmuffin'])) {
             <div class='col-sm-3'></div>
             <div class='col-sm-6'>
                 <p class='lead'>
-                    If I cared more I'd think up a scathing remark
-                </p>
-            </div>
-            <div class='col-sm-3'></div>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
-</html>
-    <?php
-} else {
-    ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="css/style.css" rel="stylesheet">
-    <title>User:TheresNoTime</title>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-205260692-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-205260692-1');
-    </script>
-  </head>
-  <body>
-    <div class='container text-center'>
-        <div class='row'>
-            <div class='col-sm pt-5'>
-                <h1 class='display-2'>Hey there! 👋</h1>
-            </div>
-        </div>
-
-        <div class='row'>
-            <div class='col'></div>
-            <div class='col'>
-                <img src='img/pfp5.png' class='img-fluid rounded'>
-            </div>
-            <div class='col'></div>
-        </div>
-
-        <div class='row pt-3'>
-            <div class='col-sm-3'></div>
-            <div class='col-sm-6'>
-                <p class='lead'>
-                    I'm <a href='https://en.wikipedia.org/wiki/User:TheresNoTime' target='_blank'>TheresNoTime</a>, but the name I prefer to go by (when <i>not</i> having to put up with terrible puns about my username) is <strong>Sam</strong>.
+                    I'm Sam, but you might know me as <a href='https://en.wikipedia.org/wiki/User:TheresNoTime' target='_blank'>TheresNoTime</a> 😅
                 </p>
                 <p>
-                    I’m from the South of England, do unspeakable things to technology and <s>enjoy long walks on the beach</s>.
+                    I’m from the South of England, I do unspeakable things to technology and I edit Wikipedia.
                 </p>
             </div>
             <div class='col-sm-3'></div>
@@ -101,17 +48,41 @@ if (isset($_GET['susmuffin'])) {
         <div class='row'>
             <div class='col'>
                 <p>
-                    [[<a href='https://twitter.com/TheresNoTimeFor' target='_blank'>Twitter</a>]]
-                    [[<a href='https://www.theresnotime.co.uk/words/' target='_blank'>Blog</a>]]
-                    [[<a href='https://discord.gg/HDkHHRRs' target='_blank'>Discord Server!</a>]]
-                    [[<a href='https://tools.theresnotime.co.uk' target='_blank'>Tools</a>]]
+                    . . . .
                 </p>
             </div>
         </div>
+
+        <div class='row'>
+            <div class='col'>
+                <p>
+                    While you're here, you should...
+                </p>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col'>
+                <p>
+                    [[ <a href='https://twitter.com/TheresNoTimeFor' target='_blank'>Follow me on Twitter</a> ]]
+                </p>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col'>
+                <p>
+                    [[ <a href='https://www.theresnotime.co.uk/words/' target='_blank'>Read my blog</a> ]]
+                </p>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col'>
+                <p>
+                    [[ <a href='https://discord.gg/HDkHHRRs' target='_blank'>Join my Discord Server!</a> ]]
+                </p>
+            </div>
+        </div>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
-    <?php
-}
-?>
