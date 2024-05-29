4e3264346b529804a32868c6bf1e417d07603960
TheresNoTime <sam@theresnotime.co.uk>
2024-05-29 10:30:09 +0100

    Start auto page idea

---
+++ scripts/new.js
@@ -0,0 +1,36 @@
+let template_top = `<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Boring disclaimers</title>
+        <meta name="robots" content="index, follow" />
+        <meta
+            name="description"
+            content="Haven't I got some boring af disclaimers for you!" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Boring disclaimers" />
+        <meta
+            property="og:url"
+            content="https://www.theresnotime.co.uk/disclaimers" />
+        <meta
+            property="og:description"
+            content="Haven't I got some boring af disclaimers for you!" />
+        <meta property="og:image" content="/img/pfp.png" />
+        <link href="css/base.css" rel="stylesheet" />
+        <link href="css/monokai.css" rel="stylesheet" />
+        <link href="css/overrides.css" rel="stylesheet" />
+        <style>
+            .disclaimer {
+                text-align: initial;
+            }
+        </style>
+    </head>
+    <body>
+        <main>
+            <div id="profile-picture-container">
+                <img
+                    alt="A fox emoji"
+                    id="profile-picture"
+                    src="/img/pfp.png" />
+            </div>`
